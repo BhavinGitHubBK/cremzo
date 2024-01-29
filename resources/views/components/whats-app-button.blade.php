f842066472cbbc4146100df0ea9ba3b05bc31073
Bhavin K. <bapatel183@gmail.com>
2024-01-29 23:26:09 +0530

    new changes

---
+++ resources/views/components/whats-app-button.blade.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="{{ asset('/public/backend/assets/fonts/icofont/icofont.min.css') }}">
+
+<div style="float: right; padding: 65px 67px 0 0;">
+    <a href="https://wa.me/{{ $phoneNumber }}" target="_blank" class="whatsapp-button btn-circle custom-icon-btn">
+        <i class="icofont-whatsapp" style="font-size: x-large; float: right;"></i>
+    </a>
+</div>
--- themes/tlcommerce/resources/views/frontend/layouts/master.blade.php
+++ themes/tlcommerce/resources/views/frontend/layouts/master.blade.php
@@ -1,32 +1,32 @@
 @php
-    use Plugin\TlcommerceCore\Repositories\SettingsRepository;
-    use Core\Repositories\SettingsRepository as CoreSettingRepository;
-    use Illuminate\Support\Facades\Cache;
+use Plugin\TlcommerceCore\Repositories\SettingsRepository;
+use Core\Repositories\SettingsRepository as CoreSettingRepository;
+use Illuminate\Support\Facades\Cache;
 
-    $siteProperties = Cache::rememberForever('site-properties', function () {
-        return CoreSettingRepository::SiteProperties();
-    });
+$siteProperties = Cache::rememberForever('site-properties', function () {
+return CoreSettingRepository::SiteProperties();
+});
 
-    $site_name = str_replace('"', '', $siteProperties['site_title']);
-    $site_name = str_replace("'", '', $site_name);
+$site_name = str_replace('"', '', $siteProperties['site_title']);
+$site_name = str_replace("'", '', $site_name);
 
-    $site_moto = $siteProperties['site_motto'] != null ? '|' . $siteProperties['site_motto'] : '';
-    $site_title = $site_name . '' . $site_moto;
+$site_moto = $siteProperties['site_motto'] != null ? '|' . $siteProperties['site_motto'] : '';
+$site_title = $site_name . '' . $site_moto;
 
-    $default_language = defaultLanguage();
-    $default_curency = SettingsRepository::defaultCurrency();
+$default_language = defaultLanguage();
+$default_curency = SettingsRepository::defaultCurrency();
 
-    $active_theme = getActiveTheme();
+$active_theme = getActiveTheme();
 
-    $body_typography = themeOptionToCss('body_typography', $active_theme->id);
-    $paragraph_typography = themeOptionToCss('paragraph_typography', $active_theme->id);
-    $heading_typography = themeOptionToCss('heading_typography', $active_theme->id);
-    $menu_typography = themeOptionToCss('menu_typography', $active_theme->id);
-    $button_typography = themeOptionToCss('button_typography', $active_theme->id);
-    $logo_details = getGeneralSettingsDetails();
-    $custom_js_properties = getThemeOption('custom_js', $active_theme->id);
-    $site_mood_setting = getThemeOption('dark_light_switcher', $active_theme->id);
-    $site_default_mood = isset($site_mood_setting['site_default_screen_mood']) && $site_mood_setting['site_default_screen_mood'] == 'dark' ? 'dark' : '';
+$body_typography = themeOptionToCss('body_typography', $active_theme->id);
+$paragraph_typography = themeOptionToCss('paragraph_typography', $active_theme->id);
+$heading_typography = themeOptionToCss('heading_typography', $active_theme->id);
+$menu_typography = themeOptionToCss('menu_typography', $active_theme->id);
+$button_typography = themeOptionToCss('button_typography', $active_theme->id);
+$logo_details = getGeneralSettingsDetails();
+$custom_js_properties = getThemeOption('custom_js', $active_theme->id);
+$site_mood_setting = getThemeOption('dark_light_switcher', $active_theme->id);
+$site_default_mood = isset($site_mood_setting['site_default_screen_mood']) && $site_mood_setting['site_default_screen_mood'] == 'dark' ? 'dark' : '';
 @endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
@@ -35,9 +35,9 @@
     <meta charset="utf-8">
     <link rel="manifest" href="/manifest.json">
     @if (isset($logo_details['favicon']))
-        <link rel="shortcut icon" href="{{ project_asset($logo_details['favicon']) }}">
+    <link rel="shortcut icon" href="{{ project_asset($logo_details['favicon']) }}">
     @else
-        <link rel="shortcut icon" href="{{ asset('/public/backend/assets/img/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('/public/backend/assets/img/favicon.png') }}">
     @endif
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -54,8 +54,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('/public/backend/assets/plugins/fontawsome/css/all.min.css') }}">
     <link rel="stylesheet" href="/themes/tlcommerce/public/blog/css/font-awesome.min.css">
@@ -74,19 +73,19 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('/themes/tlcommerce/public/css/social_icon.css') }}">
     <!-- Including all google fonts link -->
     @includeIf('theme/tlcommerce::frontend.blog.includes.custom.google-font-link', [
-        'body_typography' => $body_typography,
-        'paragraph_typography' => $paragraph_typography,
-        'heading_typography' => $heading_typography,
-        'menu_typography' => $menu_typography,
-        'button_typography' => $button_typography,
+    'body_typography' => $body_typography,
+    'paragraph_typography' => $paragraph_typography,
+    'heading_typography' => $heading_typography,
+    'menu_typography' => $menu_typography,
+    'button_typography' => $button_typography,
     ])
     <!-- Including all dynamic css -->
     @includeIf('theme/tlcommerce::frontend.blog.includes.custom.tl-dynamic-css', [
-        'body_typography' => $body_typography,
-        'paragraph_typography' => $paragraph_typography,
-        'heading_typography' => $heading_typography,
-        'menu_typography' => $menu_typography,
-        'button_typography' => $button_typography,
+    'body_typography' => $body_typography,
+    'paragraph_typography' => $paragraph_typography,
+    'heading_typography' => $heading_typography,
+    'menu_typography' => $menu_typography,
+    'button_typography' => $button_typography,
     ])
     <!-- Theme Option Css -->
 
@@ -94,12 +93,13 @@
     @yield('builder-css-link')
     <!--Custom script-->
     @if ($custom_js_properties != null)
-        {!! $custom_js_properties['header_custom_js_code'] !!}
+    {!! $custom_js_properties['header_custom_js_code'] !!}
     @endif
     <!--End custom script-->
 </head>
 
 <body class="antialiased">
+<x-whatsApp-button :phoneNumber="+919599559155" />
     <div id="app">
     </div>
     <link rel="stylesheet" type="text/css" href="{{ asset('/themes/tlcommerce/public/css/custom_css.css') }}">
@@ -140,12 +140,12 @@
     </script>
     <!--Custom script-->
     @if ($custom_js_properties != null)
-        {!! $custom_js_properties['footer_custom_js_code'] !!}
+    {!! $custom_js_properties['footer_custom_js_code'] !!}
     @endif
     <!--End custom script-->
 
     @if (isActivePluging('tlecommercecore'))
-        <script src="{{ asset('/themes/tlcommerce/public/js/main.js?v=223') }}"></script>
+    <script src="{{ asset('/themes/tlcommerce/public/js/main.js?v=223') }}"></script>
     @endif
 
 </body>
